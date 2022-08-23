908811ee9e3b7349b34ee7d29fef7258f1eed7d8
Praneeth <praneethkrishna27@hotmail.com>
2022-08-23 20:19:49 +0530

    added PCA logic

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -1,6 +1,11 @@
 from sklearn.model_selection import train_test_split
 from sklearn import preprocessing
 from sklearn.impute import SimpleImputer
+from sklearn.decomposition import PCA
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import confusion_matrix
+from sklearn.metrics import accuracy_score
+
 
 def split_data(X, y, test_per, random_state):
      """
@@ -21,9 +26,39 @@ def normalise_data(X_train, X_test):
      return normalized_train_X, normalized_test_X
 
 def simple_impute_missing_data(X_train, X_test, col_name, strategy):
+     """
+     this function is used to impute the missing data 
+     """
      mean_imputer = SimpleImputer(strategy=strategy)
      X_train.iloc[:,col_name] = mean_imputer.fit_transform(X_train.iloc[:,col_name].values.reshape(-1,1))
      X_test.iloc[:,col_name] = mean_imputer.fit_transform(X_test.iloc[:,col_name].values.reshape(-1,1))
 
      return X_train, X_test
+
+
+def dimentionality_reduction(X_train, X_test, n_comp):
+     """
+     this function helps in reducing the dimentions to a desired n_components value
+     """
+     pca = PCA(n_comp)    
+     X_train = pca.fit_transform(X_train)
+     X_test = pca.transform(X_test)
+     explained_variance = pca.explained_variance_ratio_
+
+     # print(X_train, X_test, explained_variance)
+     return(X_train, X_test, explained_variance)
+
+
+def model_train(X_train, X_test, y_train, y_test):
+     classifier = RandomForestClassifier(max_depth=2, random_state=0)
+     classifier.fit(X_train, y_train)
+
+     # Predicting the Test set results
+     y_pred = classifier.predict(X_test)
+     print(len(y_pred))
+
+     cm = confusion_matrix(y_test, y_pred)
+     print(cm)
+     print('Accuracy' , accuracy_score(y_test, y_pred))
+
 
--- main.py
+++ main.py
@@ -1,11 +1,14 @@
-from data_preprocessing import simple_impute_missing_data, split_data,  normalise_data
+from data_preprocessing import simple_impute_missing_data, split_data,  normalise_data, dimentionality_reduction, model_train
 from sklearn import datasets
 import pandas as pd
+import numpy as np
 
 
-data = pd.DataFrame(datasets.load_wine().data)
+# data = pd.DataFrame(datasets.load_iris().data)
 
-print('original data shape', data.shape)
+data = pd.read_csv('classification.csv')
+
+# print('original data shape', data.head())
 
 # print(data.iloc[:,:-1])
 
@@ -13,11 +16,14 @@ if __name__ == "__main__":
 
     X, y = data.iloc[:, :-1], data.iloc[:, -1]
     X_train, X_test, y_train, y_test = split_data(X, y, 0.25, 7)
-    print(X_train.shape)
-    print(X_test.shape)
+    print('--------')
+    print(y_train)
+
     normalized_train_X, normalized_test_X = normalise_data(X_train, X_test)
     X_train, X_test = simple_impute_missing_data(X_train, X_test, 1, 'median')
+    X_train, X_test, explained_variance = dimentionality_reduction(X_train, X_test, 2)
+    print(np.array(explained_variance).sum())
+    model_train(X_train, X_test, y_train, y_test)
 
-    print('Mean Imputation')
-    print(X_train, X_test)
+    # print(X_train, X_test, explained_variance)
 
