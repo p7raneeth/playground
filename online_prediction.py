8cb61763f2145ed7a29d62fb3175bb760a02254a
Praneeth <30497716+p7raneeth@users.noreply.github.com>
2022-09-14 10:45:32 +0530

    added comments

--- online_prediction.py
+++ online_prediction.py
@@ -12,39 +12,56 @@ user_inputs = ['No',
                'Therapist',
                'Single']
 
+Attrition.No	Attrition.Yes	BusinessTravel.Travel_Frequently	BusinessTravel.Travel_Rarely	Department.Cardiology	Department.Maternity	EducationField.Life Sciences	EducationField.Medical	EducationField.Other	Gender.Female	Gender.Male	JobRole.Administrative	JobRole.Nurse	JobRole.Other	JobRole.Therapist	MaritalStatus.Divorced	MaritalStatus.Married	MaritalStatus.Single
+1               	0	                 0	                              1   	                      0	                    1	                         0	                        1	                       0	                   0             	1	         0                 	0	              0	             1                     	0	                 0                    	1
+
+In [ ]:
+
+# fillin the missing values with most_frequent value
 for i, col in enumerate(list(obj_data.columns)):
     obj_data[col].fillna(user_inputs[i], inplace=True)
 
 obj_data
 
+
+## initialize a list of 'n' zeros 
+# e.g if input is 5 then output is [0,0,0,0,0]
 def zerolistmaker(n):
     listofzeros = [0] * n
     return listofzeros
 
-## making of inp dictionary
+  
 
+# create col_list_names which contains encoded format of each column
+# E.g Attrition has two unique values: ['Yes', 'No']. So, Attrition.Yes and Attrition.No will be final column names for Attrition column
 col_list_names = []
 
 for col in list(obj_data.columns):
     for val in np.unique(obj_data[col]):
         col_list_names.append(col+'.'+val)
 
-col_list_names
 
+# creating a dictionary in such a way that user inputs are stored in a list and in integer value to specify unique values for that column
+
+## E.g if User input for Attrition is No, then the dictionary d will contain the values 
+
+# d = {'Attrition' : ['No', 2]} so on and so forth a]for all the remaining values
 d = {}
 
 for i, col in enumerate(list(obj_data.columns)):
     d[col] = [user_inputs[i], len(np.unique(obj_data[col]))]
 
+    
+
 m=0
 master_list = []
+# iterate over the user_input dictionary for all the items
 for k, v in d.items():
-#     print(m, m+v[1])
+  # make a list of required number of zero's based on unique values for a particular key in the dictionary
     answer_list = zerolistmaker(len(col_list_names[m:m+v[1]]))
     for i, val in enumerate(col_list_names[m:m+v[1]]):
-#         print(inp[k][0])
+        # only update that index value of the list where 'No' is present and leave the rest as it is 
         if d[k][0] == val.split('.')[1]:
-#             print('---i---', i)
             answer_list[i] = 1
             master_list.extend(answer_list)
     m = m + v[1]
